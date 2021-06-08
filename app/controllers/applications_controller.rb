ba50cc740cad087ab7aa61658db4ce7fede7c516
Emma Cardon <emma.cardon7@gmail.com>
2021-06-08 17:52:16 +0100

    applications controller actions and new view

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationsController < ApplicationController
+  def new
+    @project = Project.find(params[:id])
+    @application = Application.new
+  end
+
+  def create
+    @project = Project.find(params[:id])
+    @application = Application.new(application_params)
+    @application.project = @project
+    @application.save
+    if @application.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def application_params
+    params.require(:application).permit(:motivation_letter, :resume)
+  end
 end
