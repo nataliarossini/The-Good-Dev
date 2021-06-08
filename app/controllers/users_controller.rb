86f9ad62840711daf57bbfc46ba537bc0341d738
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-08 22:42:01 +0100

    added controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(pararams[:id])
   end
 
   def dashboard
+    @user = current_user
+    @applications = @user.applications
+    @projects = @user.projects
   end
 end
