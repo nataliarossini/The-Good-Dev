b271dd8adde3a8831c5b068beba6be7086ae45e2
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-08 18:48:40 +0100

    created users forms

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def dashboard
+  end
 end
