8d547b3d7b728da99d58f8985d192b9fe216efa0
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-09 13:39:32 +0100

    dont merge

--- app/models/organization.rb
+++
@@ -1,9 +0,0 @@
-class Organization < ApplicationRecord
-  belongs_to :user
-  has_many :projects
-  has_many :reviews
-  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 50 }
-  validates :registration, presence: true, uniqueness: true
-  validates :category, inclusion: { in: %w(a b c) }
-  validates :url, uniqueness: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_151533) do
+ActiveRecord::Schema.define(version: 2021_06_08_183718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,18 @@ ActiveRecord::Schema.define(version: 2021_06_08_151533) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "user_type"
+    t.text "description"
+    t.string "address"
+    t.index ["email"], name: "index_organizations_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_organizations_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_organizations_on_user_id"
   end
 
