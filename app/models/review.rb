69754900898569eab910bd3857505f67a61bcbd1
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-06-09 14:29:38 +0100

    Review controller and form

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :organization
   validates :content, presence: true, length: { minimum: 10, maximum: 120 }
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 0..5 }
-  validates :author, presence: true, length: { maximum: 20}
+  validates :author_id, presence: true, numericality: { only_integer: true }
 end
