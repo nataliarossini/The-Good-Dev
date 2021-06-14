e033c17de0426c5c920c8d7eb93f1d49fb3acc5a
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-06-14 09:10:55 +0100

    Application respond path set up and link provided on org. dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Application.destroy_all
+Review.destroy_all
 Project.destroy_all
 Organization.destroy_all
 User.destroy_all
