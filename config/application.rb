ea3ef6992418995b8af07765a23eec3884c6f5e8
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-15 20:11:40 +0100

    trued to fix issue

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module TheGoodDev
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
