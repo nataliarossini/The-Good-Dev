eddc702608f8724351d3edbf9e25a51b1f684e77
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-06-17 20:55:47 +0100

    changes for project checklist

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -4,6 +4,14 @@ module MetaTagsHelper
   def meta_title
     content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
+  
+  def meta_type
+    content_for?(:meta_type) ? content_for(:meta_type) : DEFAULT_META["meta_type"]
+  end
+
+  def meta_url
+    content_for?(:meta_url) ? content_for(:meta_url) : DEFAULT_META["meta_url"]
+  end
 
   def meta_description
     content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
